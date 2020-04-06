552ac3cf707b8fd039548399cf5d8a89356e97d4
Ryan Graves <ryan.a.graves@icloud.com>
2020-04-06 12:33:01 -0700

    Added a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :store, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
+  get  'store/show'
 end
